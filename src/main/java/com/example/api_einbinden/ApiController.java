994abef6c8263f8004c6aa1ce382ede7f00b45d5
LUEJ9 <juan.luethy@coop.ch>
2023-12-12 14:25:24 +0100

    Big Bonk

---
+++ src/main/java/com/example/api_einbinden/ApiController.java
@@ -0,0 +1,13 @@
+package com.example.api_einbinden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEinbindenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEinbindenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api_einbinden/ApiEinbindenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_einbinden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEinbindenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
